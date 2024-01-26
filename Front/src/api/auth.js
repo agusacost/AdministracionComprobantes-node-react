6d401135d56b89ab315250f2fa3d378408727c97
agusacost <juanagustinacost@gmail.com>
2024-01-26 18:10:12 -0300

    se elimina ruta de registro

--- Front/src/api/auth.js
+++ Front/src/api/auth.js
@@ -1,6 +1,5 @@
+//rutas de autenticacion
 import axios from "./axios.js";
-
-export const registerRequest = (user) => axios.post(`/register`, user);
 
 export const loginRequest = (user) => axios.post(`/login`, user);
 
