c0157fa8a3fabb294119af44bb6befb9d81de360
agusacost <juanagustinacost@gmail.com>
2024-01-25 21:23:27 -0300

    Se documenta el archivo

--- Back/middleware/validateToken.js
+++ Back/middleware/validateToken.js
@@ -1,12 +1,14 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config.js');
 
+//verifica la existencia de token para el ingreso 
 exports.authRequired = async(req,res,next)=>{
     const {token} = req.cookies;
     const TOKEN_SECRET = config.TOKEN_SECRET;
 
     if(!token) return res.status(401).json({mensaje: 'No Token, autorizacion denegada'});
 
+    //decodifica el token y verifica
     jwt.verify(token, TOKEN_SECRET, (err,user)=>{
         if(err) return res.status(403).json({mensaje:'Invalid Token'});
 
